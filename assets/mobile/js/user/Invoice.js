e34cab95ebfb6bc94a331d1b1e587c153cb7e828
Denin2004 <denin_2004@yahoo.com>
2022-05-02 15:03:10 +0300

    mobile invoices

--- assets/mobile/js/user/Invoice.js
+++ assets/mobile/js/user/Invoice.js
@@ -68,7 +68,7 @@ class Invoice extends Component {
                     </List.Item>
                 </List>
                 : null}
-                {this.state.invoice.chargeType == 2 ?
+                {this.state.invoice.chargeType == 2 || this.state.invoice.chargeType == 3 ?
                 <List header={this.props.t('calendar.months.'+this.state.invoice.month)+' '+this.state.invoice.year+' '+this.state.invoice.budgetName }>
                     <List.Item key={0} extra={<MfwNumber value={this.state.invoice.square}/>}>{this.props.t('land.square')}</List.Item>
                     <List.Item key={1} extra={<MfwNumber value={this.state.invoice.amount}/>}>{this.props.t('finance.sum')}</List.Item>
