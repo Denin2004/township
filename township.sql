f78a5dd75be487d8ee791b4f1525d2b975bb1194
denin <denin@denin-All-Series>
2022-06-07 16:38:15 +0300

    ACPWEB2-1046

---
+++ township.sql
@@ -0,0 +1,45 @@
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (86, 5756.08, 3, '2022-04-30');
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (86, 7057.08, 2, '2022-04-30');
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (57, 3478.18, 1, '2022-04-30');
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (57, 7819.33, 2, '2022-04-30');   
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (57, 6293.83, 3, '2022-04-30');      
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (190, 361.56, 2, '2022-04-30');
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (184, 14114.16, 2, '2022-04-30');
+update balances.pays set amount=2676.36 where id=391;
+update balances.pays set amount=10200 where id=585;
+update balances.pays set amount=10299 where id=585;
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (109, 0.19, 3, '2022-04-30');
+update balances.pays set amount=14114.28 where id=182;
+update balances.pays set amount=10299 where id=181;
+update balances.pays set amount=14114.28 where id=186;
+update balances.pays set amount=amount-2676.36 where id=185;
+update balances.pays set amount=amount-4539.72 where id=214;
+update balances.pays set amount=amount-2676.36 where id=213;
+update balances.pays set amount=amount-999.02 where id=553;
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (167, 3284.68, 1, '2022-04-30');
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (134, 2307.02, 1, '2022-04-30');
+update balances.pays set amount=amount+485.71 where id=530;
+update balances.pays set amount=amount+0.26 where id=518;
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (82, 885.92, 1, '2022-04-30');
+update balances.pays set amount=amount+25.79 where id=517;
+update balances.pays set amount=amount-1679.88 where id=500;
+update balances.pays set amount=amount+2341.21 where id=502;
+update balances.pays set amount=amount+365.99 where id=483;
+update balances.pays set amount=amount+139.51 where id=492;
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (36, 7.88, 1, '2022-04-30');
+insert into balances.pays (land_id, amount, charge_type_id, dt)
+   values (15, 5123, 1, '2022-04-30');
+update balances.pays set amount=amount+19.34 where id=463;
+update balances.pays set amount=amount+9284.49 where id=469;
